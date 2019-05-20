05d81021a2cdc10e37e7636243185fd93307f953
nickhuang <sef1548@gmail.com>
2019-05-20 23:22:25 +0800

    add two sum

---
+++ Two_Sum/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+
+  for (var i; i < nums.length - 1; i++) {
+    for (var j = i + 1; j < nums.length; j++) {
+      if (num[i] + num[j] = target)
+        return [i, j];
+    }
+  }
+};
